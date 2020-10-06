da8daf9989004218a9322f464421c5978c93fcb8
Danny Lin <danny@kdrag0n.dev>
2020-10-05 23:47:42 -0700

    walleye: Inherit proprietary LineageOS vendor blobs
    Change-Id: I03eff4e1844310971d6ae10d7d52d5453cb55084

--- aosp_walleye.mk
+++ aosp_walleye.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 $(call inherit-product, device/google/muskie/device-walleye.mk)
 $(call inherit-product-if-exists, vendor/google_devices/muskie/proprietary/device-vendor-walleye.mk)
+$(call inherit-product-if-exists, vendor/google/walleye/walleye-vendor.mk)
 
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0 \
