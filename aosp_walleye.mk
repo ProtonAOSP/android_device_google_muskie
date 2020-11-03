fcbcc9735163700509409fdbd8a6965979d73b45
Danny Lin <danny@kdrag0n.dev>
2020-11-03 11:28:56 -0800

    walleye: Remove stock product override name
    This is no longer necessary now that we've debranded the product name to
    match stock. Support for overriding the product name has been removed in
    the build system in favor of debranding device trees.
    
    Change-Id: I63f7e72c9bea7ed6b0f3901f62378e209e822227

--- aosp_walleye.mk
+++ aosp_walleye.mk
@@ -45,6 +45,5 @@ PRODUCT_MODEL := Pixel 2
 
 # Match stock product info
 PRODUCT_OVERRIDE_INFO := true
-PRODUCT_OVERRIDE_NAME := walleye
 PRODUCT_OVERRIDE_FINGERPRINT := google/walleye/walleye:11/RP1A.201005.004/6782484:user/release-keys
 PRODUCT_OVERRIDE_DESC := walleye-user 11 RP1A.201005.004 6782484 release-keys
