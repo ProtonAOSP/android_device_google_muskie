5f0f3a28c1a3b188d0fdabe7658b7ec9154be92f
Danny Lin <danny@kdrag0n.dev>
2020-12-15 11:35:31 -0800

    walleye: Update product info from RP1A.201005.004.A1

--- aosp_walleye.mk
+++ aosp_walleye.mk
@@ -45,5 +45,5 @@ PRODUCT_MODEL := Pixel 2
 
 # Match stock product info
 PRODUCT_OVERRIDE_INFO := true
-PRODUCT_OVERRIDE_FINGERPRINT := google/walleye/walleye:11/RP1A.201005.004/6782484:user/release-keys
-PRODUCT_OVERRIDE_DESC := walleye-user 11 RP1A.201005.004 6782484 release-keys
+PRODUCT_OVERRIDE_FINGERPRINT := google/walleye/walleye:11/RP1A.201005.004.A1/6934943:user/release-keys
+PRODUCT_OVERRIDE_DESC := walleye-user 11 RP1A.201005.004.A1 6934943 release-keys
